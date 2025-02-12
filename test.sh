9b9395af21eea5871b4e5645d0d29b7a8f5819c4
Avanish Kulkarni <bigfatnotebook@gmail.com>
2025-02-12 17:44:19 -0500

    testing script removed from gitignore

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then 
+    echo "$0 <filename>"
+    exit 1
+fi
+
+if [ main.ml -nt main ]; then
+    ocamlc main.ml -o main 
+fi
+
+cool --parse "$1"
+./main "$1-ast"

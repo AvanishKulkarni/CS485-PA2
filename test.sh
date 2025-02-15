545727c2558df35c8af7b4afc77f2082dec6f58b
acabrera04 <acabrera04164@gmail.com>
2025-02-14 21:29:40 -0500

    Updated bash script for testing if errors matched

--- test.sh
+++ test.sh
@@ -8,10 +8,18 @@ fi
 if [ main.ml -nt main ]; then
     ocamlc main.ml -o main 
 fi
+rm -f *.cl-ast
+rm -f *.cl-type
+rm -f reference_error.txt
+rm -f test_error.txt
 
 cool --parse "$1"
-./main "$1-ast"
+./main "$1-ast" > test_error.txt
 
-cool --class-map "$1" --out temp_ref
+cool --class-map "$1" --out temp_ref > reference_error.txt
 
-diff -b -B -w temp_ref.cl-type "$1-type"
+if [ -f "temp_ref.cl-type" ]; then
+    diff -b -B -w temp_ref.cl-type "$1-type"
+else
+    diff -b -B -w reference_error.txt test_error.txt
+fi

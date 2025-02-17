6cee5db4317f9d761c82c36d792c8f60141d7197
acabrera04 <acabrera04164@gmail.com>
2025-02-16 23:21:15 -0500

    Updated bash script to automatically test all test_cases if no argument is passed

--- test.sh
+++ test.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then 
-    echo "$0 <filename>"
-    exit 1
-fi
-
 if [ main.ml -nt main ]; then
     ocamlc main.ml -o main 
 fi
-rm -f *.cl-ast
-rm -f *.cl-type
-rm -f reference_error.txt
-rm -f test_error.txt
+count=0
+total=0
+function run_tests() {
+    rm -f *.cl-ast
+    rm -f *.cl-type
+    rm -f test_cases/*.cl-ast
+    rm -f test_cases/*.cl-type
+    rm -f reference_error.txt
+    rm -f test_error.txt
+    cool --parse "$1"
+    ./main "$1-ast" > test_error.txt
+    cool --class-map "$1" --out temp_ref > reference_error.txt
 
-cool --parse "$1"
-./main "$1-ast" > test_error.txt
+    if [ -f "temp_ref.cl-type" ]; then
+        diff -b -B -w temp_ref.cl-type "$1-type" > /dev/null
+    else
+        diff -b -B -w reference_error.txt test_error.txt > /dev/null
+    fi
+    if [ $? -eq 0 ]; then
+        echo "Passed $1"
+        count=$((count + 1))
+    else 
+        echo "Failed $1"
+    fi
+    total=$((total +1))
+}
 
-cool --class-map "$1" --out temp_ref > reference_error.txt
-
-if [ -f "temp_ref.cl-type" ]; then
-    diff -b -B -w temp_ref.cl-type "$1-type"
+if [ -n "$1" ]; then 
+    run_tests $1
 else
-    diff -b -B -w reference_error.txt test_error.txt
+    
+    for file in test_cases/*; do
+        run_tests $file
+        echo ""
+    done
+    echo ""
+    echo "Passed $count/$total test cases"
+    rm -f test_cases/*.cl-ast
+    rm -f test_cases/*.cl-type
 fi
